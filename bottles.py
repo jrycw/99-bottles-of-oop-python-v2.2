87b86791352fd92402097080df8ca5e642db9878
jrycw <jerry@ycwu.space>
2024-11-10 10:48:42 +0800

    Utilize BottleNumberFactory to create BottleNumber using from_number() and create_successor()

--- bottles.py
+++ bottles.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from typing import Self
 
 
 class AbstractBottleNumber(ABC):
@@ -15,26 +14,17 @@ class AbstractBottleNumber(ABC):
     @abstractmethod
     def pronoun(self) -> str: ...
 
-    @abstractmethod
-    def successor(self) -> str: ...
-
-    @staticmethod
-    def from_number(number: int) -> Self:
-        match number:
-            case 0:
-                cls_ = BottleNumber0
-            case 1:
-                cls_ = BottleNumber1
-            case 6:
-                cls_ = BottleNumber6
-            case _:
-                cls_ = BottleNumber
-        return cls_(number)
-
 
 class BottleNumber(AbstractBottleNumber):
     def __init__(self, number: int):
         self._number = number
+
+    @property
+    def number(self) -> int:
+        """
+        A public API to get the wrapped number
+        """
+        return self._number
 
     def __str__(self) -> str:
         return f"{self.quantity()} {self.container()}"
@@ -51,9 +41,6 @@ class BottleNumber(AbstractBottleNumber):
     def pronoun(self) -> str:
         return "one"
 
-    def successor(self) -> Self:
-        return self.from_number(self._number - 1)
-
 
 class BottleNumber0(BottleNumber):
     def quantity(self) -> str:
@@ -61,9 +48,6 @@ class BottleNumber0(BottleNumber):
 
     def action(self) -> str:
         return "Go to the store and buy some more"
-
-    def successor(self) -> Self:
-        return self.from_number(99)
 
 
 class BottleNumber1(BottleNumber):
@@ -82,20 +66,60 @@ class BottleNumber6(BottleNumber):
         return "six-pack"
 
 
-class BottleVerse:
-    @classmethod
-    def lyrics(cls, number: int) -> str:
-        return cls(BottleNumber.from_number(number))._lyrics()
+class BottleNumberFactory:
+    @staticmethod
+    def from_number(number: int) -> BottleNumber:
+        """
+        Make BottleNumber from an integer
+        """
+        match number:
+            case 0:
+                cls_ = BottleNumber0
+            case 1:
+                cls_ = BottleNumber1
+            case 6:
+                cls_ = BottleNumber6
+            case _:
+                cls_ = BottleNumber
+        return cls_(number)
 
+    @staticmethod
+    def create_successor(bottle_number: BottleNumber) -> BottleNumber:
+        """
+        Create the successor of a BottleNumber-ish class
+        """
+        wrapped_number = bottle_number.number  # get the wrapped number via the public API
+        match wrapped_number:
+            case 0:
+                successor_number = 99
+            case _:
+                successor_number = wrapped_number - 1
+        return BottleNumberFactory.from_number(successor_number)
+
+
+class BottleVerse:
     def __init__(self, bottle_number: BottleNumber):
         self._bottle_number = bottle_number
 
+    @property
+    def bottle_number(self) -> BottleNumber:
+        """
+        A public API to get the wrapped BottleNumber
+        """
+        return self._bottle_number
+
+    @classmethod
+    def lyrics(cls, number: int) -> str:
+        bottle_number = BottleNumberFactory.from_number(number)
+        return cls(bottle_number)._lyrics()
+
     def _lyrics(self) -> str:
+        successor = BottleNumberFactory.create_successor(self.bottle_number)
         return (
-            f"{self._bottle_number} of beer on the wall, ".capitalize()
-            + f"{self._bottle_number} of beer.\n"
-            f"{self._bottle_number.action()}, "
-            f"{self._bottle_number.successor()} of beer on the wall.\n"
+            f"{self.bottle_number} of beer on the wall, ".capitalize()
+            + f"{self.bottle_number} of beer.\n"
+            f"{self.bottle_number.action()}, "
+            f"{successor} of beer on the wall.\n"
         )
 
 
